474094eb7e5248d27fdd3d1965939a56957c8cbc
Surya985 <suryavarman357@gmail.com>
2024-12-11 19:57:59 +0800

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+// api/jarvis.js
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json({ message: 'Hello from Vercel server!' });
+  } else {
+    res.status(400).json({ message: 'Invalid request' });
+  }
+}
